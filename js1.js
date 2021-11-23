0201a0f2b62c6f2cbf3a20cb430cb09ac804b923
Ushna khan <77790509+ushnak-tech@users.noreply.github.com>
2021-11-23 11:17:54 +0530

    Add files via upload

---
+++ js1.js
@@ -0,0 +1,17 @@
+names=['Ali','Raj','Cinderalla','Thomas','Stuart'];
+function greet(names,smn) {
+    for (let index = 0; index < names.length; index++) {
+        const element = names[index];
+        
+    
+    console.log(smn+element+' is actually a good person');
+    
+}
+}
+let greetText=' Assalamu alaikum '
+greet(names,greetText);
+function sum(a,b,c){
+    return a*b*c
+}
+let ssuumm=sum(3,5,7)
+console.log(ssuumm)
